2f1f3dd1b29639ff79ce736d8bd0c1f3bef9bcc2
Maximilian Flügel <fluegel@hypros.de>
2022-09-10 13:54:06 +0200

    Start creating configuration component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ConfigurationCard from './configuration/ConfigurationCard';
+import './App.css';
 
 export default () => {
     return (
-        <div>Hello World!</div>
+        <div className="root-container ui container">
+            <h2>Aruba Access Point SSH Poll Client</h2>
+            <div className="ui cards">
+                <ConfigurationCard />
+            </div>
+        </div>
     );
 };
